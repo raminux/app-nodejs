03133ac1b5f797503c2d5adbabd5d77680bcb714
Ramin Esmzad <ramin.esmzad@hotmail.com>
2023-03-12 22:32:33 +0000

    Adding the favorite flag

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -43,21 +43,25 @@ export default class MovieService {
     // TODO: Open an Session
     const session = this.driver.session()
     // TODO: Execute a query in a new Read Transaction
-    const res = await session.readTransaction(
-      tx => tx.run(
-        `
+// Execute a query in a new Read Transaction
+  const res = await session.executeRead(
+  async tx => {
+    const favorites = await this.getUserFavorites(tx, userId)
+
+    return tx.run(
+      `
         MATCH (m:Movie)
         WHERE m.\`${sort}\` IS NOT NULL
         RETURN m {
-          .*
+          .*,
+          favorite: m.tmdbId IN $favorites
         } AS movie
         ORDER BY m.\`${sort}\` ${order}
         SKIP $skip
         LIMIT $limit
-        `, 
-        {skip: int(skip), limit: int(limit)}
-      )
-    )
+      `, { skip: int(skip), limit: int(limit), favorites })
+  }
+)
     // TODO: Get a list of Movies from the Result
     const movies = res.records.map(
       row => toNativeTypes(row.get('movie'))
@@ -224,7 +228,23 @@ export default class MovieService {
    */
   // tag::getUserFavorites[]
   async getUserFavorites(tx, userId) {
+      // If userId is not defined, return an empty array
+  if ( userId === undefined ) {
     return []
+  }
+
+  const favoriteResult = await tx.run(
+    `
+      MATCH (:User {userId: $userId})-[:HAS_FAVORITE]->(m)
+      RETURN m.tmdbId AS id
+    `,
+    { userId, }
+  )
+
+  // Extract the `id` value returned by the cypher query
+  return favoriteResult.records.map(
+    row => row.get('id')
+  )
   }
   // end::getUserFavorites[]
 
